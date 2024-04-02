62723f2c0303d9a11ac0b246a3ba87f434243dc4
AsareBrave <asarebrave@gmail.com>
2024-04-02 20:40:28 +0100

    init: set up the repo

--- src/features/user/ForgotPassword.js
+++ src/features/user/ForgotPassword.js
@@ -1,6 +1,5 @@
 import {useState, useRef} from 'react'
 import {Link} from 'react-router-dom'
-import LandingIntro from './LandingIntro'
 import ErrorText from  '../../components/Typography/ErrorText'
 import InputText from '../../components/Input/InputText'
 import CheckCircleIcon  from '@heroicons/react/24/solid/CheckCircleIcon'
@@ -36,11 +35,8 @@ function ForgotPassword(){
 
     return(
         <div className="min-h-screen bg-base-200 flex items-center">
-            <div className="card mx-auto w-full max-w-5xl  shadow-xl">
-                <div className="grid  md:grid-cols-2 grid-cols-1  bg-base-100 rounded-xl">
-                <div className=''>
-                        <LandingIntro />
-                </div>
+            <div className="card mx-auto w-full max-w-md  shadow-xl">
+                <div className=" bg-base-100 rounded-xl">
                 <div className='py-24 px-10'>
                     <h2 className='text-2xl font-semibold mb-2 text-center'>Forgot Password</h2>
 
@@ -70,8 +66,6 @@ function ForgotPassword(){
 
                                 <ErrorText styleClass="mt-12">{errorMessage}</ErrorText>
                                 <button type="submit" className={"btn mt-2 w-full btn-primary" + (loading ? " loading" : "")}>Send Reset Link</button>
-
-                                <div className='text-center mt-4'>Don't have an account yet? <Link to="/register"><button className="  inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">Register</button></Link></div>
                             </form>
                         </>
                     }
--- src/features/user/Login.js
+++ src/features/user/Login.js
@@ -1,6 +1,5 @@
 import {useState, useRef} from 'react'
 import {Link} from 'react-router-dom'
-import LandingIntro from './LandingIntro'
 import ErrorText from  '../../components/Typography/ErrorText'
 import InputText from '../../components/Input/InputText'
 
@@ -37,11 +36,9 @@ function Login(){
 
     return(
         <div className="min-h-screen bg-base-200 flex items-center">
-            <div className="card mx-auto w-full max-w-5xl  shadow-xl">
-                <div className="grid  md:grid-cols-2 grid-cols-1  bg-base-100 rounded-xl">
-                <div className=''>
-                        <LandingIntro />
-                </div>
+            <div className="card mx-auto w-full max-w-md flex justify-center shadow-xl">
+                <div className="bg-base-100 rounded-xl">
+               
                 <div className='py-24 px-10'>
                     <h2 className='text-2xl font-semibold mb-2 text-center'>Login</h2>
                     <form onSubmit={(e) => submitForm(e)}>
@@ -59,8 +56,6 @@ function Login(){
 
                         <ErrorText styleClass="mt-8">{errorMessage}</ErrorText>
                         <button type="submit" className={"btn mt-2 w-full btn-primary" + (loading ? " loading" : "")}>Login</button>
-
-                        <div className='text-center mt-4'>Don't have an account yet? <Link to="/register"><span className="  inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">Register</span></Link></div>
                     </form>
                 </div>
             </div>
