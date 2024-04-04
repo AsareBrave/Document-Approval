ffd73f4cf4a9fc9766a2a6ffd515a79440f5d2fe
AsareBrave <asarebrave@gmail.com>
2024-04-04 23:33:51 +0100

    feat:adding a page where the student can view and add document

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,33 +1,42 @@
 // All components mapping with path for internal routes
 
-
 import { lazy } from "react";
-
 
 const Dashboard = lazy(() => import("../pages/protected/Dashboard"));
 const HodDashboard = lazy(() => import("../pages/protected/HodDashboard"));
 const DeanDashboard = lazy(() => import("../pages/protected/DeanDashboard"));
-// const LecturerDashboard = lazy(() =>
-//  import("../pages/protected/LecturerDashboard")
-// );
+const LecturerDashboard = lazy(
+  () => import("../pages/protected/lecturerDashboard")
+);
+const Leads = lazy(() => import('../pages/protected/Leads'))
+
 
 const routes = [
- {
-  path: "/dashboard", // the url
-  component: Dashboard // view rendered
- },
- {
-  path: "/dashboard/hod", // the url
-  component: HodDashboard // view rendered
- },
- {
-  path: "/dashboard/dean", // the url
-  component: DeanDashboard // view rendered
- },
- {
-  // path: "/dashboard/lecturer", // the url
-  // component: LecturerDashboard // view rendered
- }
+  {
+    path: "/dashboard", // the url
+    component: Dashboard, // view rendered
+  },
+  {
+    path: "/dashboard/hod", // the url
+    component: HodDashboard, // view rendered
+  },
+  {
+    path: "/dashboard/dean", // the url
+    component: DeanDashboard, // view rendered
+  },
+  {
+    path: "/dashboard/lecturer", // the url
+    component: LecturerDashboard, // view rendered
+  },
+  {
+    path: "/dashboard/lecturer", // the url
+    component: LecturerDashboard, // view rendered
+  },
+
+  {
+    path: "/dashboard/leads", // the url
+    component: Leads, // view rendered
+  },
 ];
 
 export default routes;
--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -1,5 +1,7 @@
 /** Icons are imported separatly to reduce build time */
 import Squares2X2Icon from "@heroicons/react/24/outline/Squares2X2Icon";
+import InboxArrowDownIcon from '@heroicons/react/24/outline/InboxArrowDownIcon'
+
 
 const iconClasses = `h-6 w-6`;
 
@@ -10,9 +12,9 @@ const routes = [
   name: "Dashboard"
  },
  {
-  path: "/app/documents",
-  icon: <Squares2X2Icon className={iconClasses} />,
-  name: "Documents"
+  path: '/app/leads', // url
+  icon: <InboxArrowDownIcon className={iconClasses}/>, // icon component
+  name: 'Leads', // name that appear in Sidebar
  }
 ];
 
@@ -22,6 +24,12 @@ export const hodRoutes = [
   icon: <Squares2X2Icon className={iconClasses} />,
   name: "Dashboard"
  },
+
+ {
+  path: '/app/leads', // url
+  icon: <InboxArrowDownIcon className={iconClasses}/>, // icon component
+  name: 'Leads', // name that appear in Sidebar
+ }
 ];
 
 export const DeanRoutes = [
